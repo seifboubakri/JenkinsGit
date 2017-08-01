18f1cb6b903b6566295779b27db1cf466cf43d2e
Seif Boubakri <saif.boubakri@gmail.com>
2017-08-01 16:49:08 +0100

    test Jenkins

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+
+
+public class Hello {
+	
+
+	public static void main(String[] args) {
+		/*int nb=5;
+	
+			for (int i=0;i<nb;i++){
+				System.out.println("Hello .."+i);
+			}
+		
+*/
+		System.out.println("Hello ..");
+	}
+
+
+
+}
